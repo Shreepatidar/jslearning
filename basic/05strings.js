e6da2a040523b8146c629d33b6cfc5d806852cf8
Shreevallabh Patidar <shripatidar5@gmail.com>
2025-09-27 18:08:06 +0000

    added file

--- basic/05strings.js
+++ basic/05strings.js
@@ -2,12 +2,12 @@
 // const repoCount = 50
 // console.log(name + repoCount + " Value"); //string number string
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-// const gameName = new String('hitesh-hc-com')
+const gameName = new String('hitesh-hc-com')
 
-// // console.log(gameName[0]);
-// // console.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
 
 // // console.log(gameName.length);
