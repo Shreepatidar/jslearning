69efd281116f75bec433539aa33835c15db9db83
Shreevallabh Patidar <shripatidar5@gmail.com>
2025-09-27 12:38:53 +0000

    all

---
+++ basic/conversion.js
@@ -0,0 +1,104 @@
+// let num = "1234"
+// //console.log(typeof num)
+// let valueInNumber = Number(num)
+// //console.log(valueInNumber)
+// //console.log(typeof valueInNumber)
+
+// let str = "abcd"
+// console.log(typeof str) //string
+// let strInNumber = Number(str)
+// console.log(strInNumber)  //NaN
+// console.log(typeof strInNumber) //number
+
+// let flag = false
+// console.log(typeof flag) //boolean
+// let flagInNumber = Number(flag)
+// console.log(flagInNumber)  //0
+// console.log(typeof flagInNumber) //number
+
+// //boolean from number
+// let num1 = 0
+// console.log(typeof num1) //number
+// let numInBoolean = Boolean(num1)
+// console.log(numInBoolean)  //true
+// console.log(typeof numInBoolean) //boolean
+
+// //boolean from string 
+// let str1 = "0"
+// console.log(typeof str1) //string
+// let strInBoolean = Boolean(str1)
+// console.log(strInBoolean)  //true
+// console.log(typeof strInBoolean) //boolean
+
+
+// *********************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3); //left to right
+
+// console.log(+"2"); //unary plus operator ,output will be number
+// console.log(+"-2"); //unary plus operator ,output will be number
+// console.log(+"2.5"); //unary plus operator ,output will be number
+// console.log(+"-2.5"); //unary plus operator ,output will be number
+// console.log(+"0xff");  //255
+// console.log(+"0b11");  //3
+// console.log(+"0o11");  //9
+// console.log(+"abc");   //NaN
+// console.log(+"");      //0
+// console.log(+" ");     //0
+// console.log(+null);    //0
+// console.log(+undefined); //NaN
+// console.log(+false);   //0
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.table([num1, num2, num3]);
+console.log(num1, num2, num3);
+
+// //assignment operator
+
+// let a = 1
+// let b = 2
+// let c = 3 - (a = b + 1)
+// console.log(a, b, c);
+
+// //increment and decrement
+// let gameCounter = 100
+// gameCounter = gameCounter + 1
+// console.log(gameCounter);
+
+// gameCounter += 1
+// console.log(gameCounter);
+
+// gameCounter++
+// console.log(gameCounter);
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// // link to study
+// // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
