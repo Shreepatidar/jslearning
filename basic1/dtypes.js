e1d5889c54cf06617e0618cf68ba4b618ee9111f
Shreevallabh Patidar <shripatidar5@gmail.com>
2025-09-27 08:57:23 +0000

    dtypes

---
+++ basic1/dtypes.js
@@ -0,0 +1,26 @@
+"use strict"
+/*
+Enables strict mode, a safer version of JavaScript.
+Purpose: Catch errors and prevent bad coding practices.
+*/
+
+//alert(3+3)
+
+//console.log(3+3) console.log("wont run")
+
+console.log(3+3); console.log("will run") //but bad practice, readability nhi he
+
+console.log(3
+    +3
+)  //will work but code readability should be high
+
+
+console.log(3+3) //output 6
+console.log("best practice")
+
+let name = "shree"
+let age = 33
+let isLoggedIn = false
+
+// number  range upto 2 to the pow 53 
+
