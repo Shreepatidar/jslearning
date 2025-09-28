3ee94ef0164c0b7e7633e8b7e4b84ec2589e8772
Shreevallabh Patidar <shripatidar5@gmail.com>
2025-09-28 09:16:12 +0000

    updated thi s

---
+++ basic/05datatype-summary.js
@@ -0,0 +1,99 @@
+//  Primitive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"]; //array
+
+// let myObj = {   //object
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+/*
+typeof of variables in JavaScript 
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+
+
+*****************
+JavaScript is a dynamically typed language. This means that variable types are determined at runtime, and you do not need to explicitly declare the type of a variable before using it. You can assign different types of values to a variable during its lifetime.
+
+For example, in JavaScript, you can do the following:
+
+let x = 10; // x is now a number
+x = "Hello"; // x is now a string
+x = true; // x is now a boolean
+On the other hand, statically typed languages require you to declare the variable's type explicitly, and the type checking is done at compile-time, before the code is executed.
+
+Languages like Java, C++, and TypeScript are statically typed, and they require you to specify the variable type explicitly when declaring them:
+
+int x = 10; // x is a variable of type int
+String name = "John"; // name is a variable of type String
+JavaScript's dynamic typing allows for more flexibility but can lead to potential runtime errors if not handled carefully. Static typing, on the other hand, provides better type safety at the cost of some initial verbosity and strictness.
+
+*/
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//*********************************
+
+//stack is allocated to primitive data tyes:string, number, bigint,boolean, symbol, null, undefined,
+//heap is allocated to non primitive datatypes: array, object, function
+
+//-----------------------
+// let name = "shree"
+
+// let anotherName = name
+// anotherName = "vallabh"
+
+// console.log(anotherName)
+// console.log(name)
+
+//-----------------------
+let userone={
+    id : 1,
+    firstname : "shree"
+}
+let usertwo = userone
+
+usertwo.firstname = "shreevallabh"
+
+console.log(userone.firstname);
+console.log(usertwo.firstname);
